1da1816a66c60eaa4e82e5c63d8aa681668c83e9
Rogério Soares <rgrsoares@yahoo.com.br>
2024-01-30 10:57:52 -0300

    Alter home page with layout base

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', exphbs.engine({ 
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials')
+}));
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('home', { layout: 'main' })
 })
 
 app.listen(port, () => {
